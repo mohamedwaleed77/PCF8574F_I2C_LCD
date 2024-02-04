cc9928ec9e113aed98340e7221b34d8d11b3051e
mohamedwaleed77 <139256760+mohamedwaleed77@users.noreply.github.com>
2024-02-04 11:29:30 +0200

    Update LDCI2C_CFG.h

--- i2c_lcd/HAL/LDCI2C_CFG.h
+++ i2c_lcd/HAL/LDCI2C_CFG.h
@@ -4,9 +4,9 @@
 #define LCD_RW_PIN_ID                  1
 #define LCD_E_PIN_ID                   2
 //LCD COMMANDS:
-#define LCD_GO_TO_HOME                 	0x02
-#define LCD_CLEAR_COMMAND              	0x01
-#define LCD_BACKLIGHT					0x08
+#define LCD_GO_TO_HOME              0x02
+#define LCD_CLEAR_COMMAND          	0x01
+#define LCD_BACKLIGHT					      0x08
 #define LCD_CGRAM_START          		0x40
 #define LCD_SET_CURSOR_LOCATION  		0x80
 
