48fbe1b7be1d010c18e7aad36791b0c4706cd352
mohamedwaleed77 <139256760+mohamedwaleed77@users.noreply.github.com>
2024-02-04 11:28:42 +0200

    Update TWI_interface.h

--- i2c_lcd/HAL/TWI_interface.h
+++ i2c_lcd/HAL/TWI_interface.h
@@ -4,22 +4,14 @@
 
 #include "STD_TYPES.h"
 
-/*******************************************************************************
- *                      Preprocessor Macros                                    *
- *******************************************************************************/
+#define TWI_START         0x08 
+#define TWI_REP_START     0x10 
+#define TWI_MT_SLA_W_ACK  0x18 
+#define TWI_MT_SLA_R_ACK  0x40 
+#define TWI_MT_DATA_ACK   0x28 
+#define TWI_MR_DATA_ACK   0x50 
+#define TWI_MR_DATA_NACK  0x58
 
-/* I2C Status Bits in the TWSR Register */
-#define TWI_START         0x08 /* start has been sent */
-#define TWI_REP_START     0x10 /* repeated start */
-#define TWI_MT_SLA_W_ACK  0x18 /* Master transmit ( slave address + Write request ) to slave + ACK received from slave. */
-#define TWI_MT_SLA_R_ACK  0x40 /* Master transmit ( slave address + Read request ) to slave + ACK received from slave. */
-#define TWI_MT_DATA_ACK   0x28 /* Master transmit data and ACK has been received from Slave. */
-#define TWI_MR_DATA_ACK   0x50 /* Master received data and send ACK to slave. */
-#define TWI_MR_DATA_NACK  0x58 /* Master received data but doesn't send ACK to slave. */
-
-/*******************************************************************************
- *                      Functions Prototypes                                   *
- *******************************************************************************/
 void TWI_init(void);
 void TWI_start(void);
 void TWI_stop(void);
